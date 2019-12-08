8c14969986fc93d6d7ee9f07867e48edea96e549
mdarfay <mdarfay@insa-rennes.fr>
2019-12-08 19:27:33 +0100

    refactor(Tools): Surcharge of the function regionOfInterest with different parameters

--- app/src/tools.cpp
+++ app/src/tools.cpp
@@ -1,8 +1,12 @@
 #include "tools.hpp"
 
-Mat regionOfInterest(const Mat img, const Point2i topLeft, const Point2i bottomRight) {
-    int width = abs(topLeft.x - bottomRight.x);
-    int height = abs(topLeft.y - bottomRight.y);
-    Rect roi = Rect(topLeft.x, topLeft.y, width, height);
+Mat regionOfInterest(const Mat img, const int xTopLeft, const int yTopLeft, const int xBottomRight, const int yBottomRight) {
+    int width = abs(xTopLeft - xBottomRight);
+    int height = abs(yTopLeft - yBottomRight);
+    Rect roi = Rect(xTopLeft, yTopLeft, width, height);
     return Mat(img, roi);
+}
+
+Mat regionOfInterest(const Mat img, const Point2i topLeft, const Point2i bottomRight) {
+    return regionOfInterest(img, topLeft.x, topLeft.y, bottomRight.x, bottomRight.y);
 }
