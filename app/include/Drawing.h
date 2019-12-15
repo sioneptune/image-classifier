59d12a209516a5fe515a70eb08626fbf2d9fec9c
mdarfay <mdarfay@insa-rennes.fr>
2019-12-15 17:28:44 +0100

    feat(Drawing): Add class to represent the drawings and their description

---
+++ app/include/Drawing.h
@@ -0,0 +1,54 @@
+#ifndef IMAGE_CLASSIFIER_DRAWING_H
+#define IMAGE_CLASSIFIER_DRAWING_H
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+#include "opencv2/imgproc.hpp"
+
+using namespace cv;
+
+class Drawing {
+
+private:
+    Mat img;
+    string label;
+    string form;
+    string scripter;
+    string page;
+    int row;
+    int column;
+    string size;
+
+public:
+    Drawing(const string &form);
+
+    const Mat& getImg() const;
+    void setImg(const Mat &img);
+
+    const string& getLabel() const;
+    void setLabel(const string &label);
+
+    const string& getForm() const;
+
+    const string& getScripter() const;
+
+    const string& getPage() const;
+
+    int getRow() const;
+    void setRow(int row);
+
+    int getColumn() const;
+    void setColumn(int column);
+
+    const string& getSize() const;
+    void setSize(const string &size);
+
+};
+
+ofstream& operator<<(ofstream& ofs, const Drawing d);
+
+
+#endif //IMAGE_CLASSIFIER_DRAWING_H
---
+++ app/src/Drawing.cpp
@@ -0,0 +1,82 @@
+#include "Drawing.h"
+
+Drawing::Drawing(const string &form) : form(form) {
+    scripter = form.substr(0, 3);
+    page = form.substr(3, 2);
+}
+
+ofstream& operator<<(ofstream& ofs, const Drawing d) {
+    ofs << "label " << d.getLabel() << endl
+    << "form " << d.getForm() << endl
+    << "scripter " << d.getScripter() << endl
+    << "page " << d.getPage() << endl
+    << "row " << d.getRow() << endl
+    << "column " << d.getColumn() << endl
+    << "size " << d.getSize() << endl;
+    return ofs;
+}
+
+
+const Mat& Drawing::getImg() const {
+    return img;
+}
+
+void Drawing::setImg(const Mat &img) {
+    Drawing::img = img;
+}
+
+const string& Drawing::getLabel() const {
+    return label;
+}
+
+void Drawing::setLabel(const string &label) {
+    Drawing::label = label;
+}
+
+const string& Drawing::getForm() const {
+    return form;
+}
+
+const string& Drawing::getScripter() const {
+    return scripter;
+}
+
+const string& Drawing::getPage() const {
+    return page;
+}
+
+int Drawing::getRow() const {
+    return row;
+}
+
+void Drawing::setRow(int row) {
+    Drawing::row = row;
+}
+
+int Drawing::getColumn() const {
+    return column;
+}
+
+void Drawing::setColumn(int column) {
+    Drawing::column = column;
+}
+
+const string& Drawing::getSize() const {
+    return size;
+}
+
+void Drawing::setSize(const string &size) {
+    Drawing::size = size;
+}
+
+
+#include "tools.hpp"
+
+int _main(int argc, char **argv) {
+    Drawing d = Drawing("00102");
+    d.setColumn(2);
+    d.setRow(1);
+    d.setLabel("fire");
+    d.setSize("small");
+    saveImgDesc("../../../data/imgdesc.txt", d);
+}
