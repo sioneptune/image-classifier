b3ec0464aea3ca5a48826aabcdc6f7b2fc493fa5
mdarfay <mdarfay@insa-rennes.fr>
2019-12-17 08:24:44 +0100

    feat(Tools): Add a Timer

--- app/include/tools.h
+++ app/include/tools.h
@@ -2,6 +2,7 @@
 #define IMAGE_CLASSIFIER_TOOLS_H
 
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
@@ -29,5 +30,40 @@ void saveImg(const string name, const Mat& img);
 void saveImgDesc(const string name, const Drawing& d);
 
 Mat openImage(const String path);
+
+
+class Timer
+{
+    std::chrono::steady_clock::time_point start;
+    std::chrono::steady_clock::time_point end;
+public:
+    Timer() :start(std::chrono::steady_clock::now()) {};
+    Timer& set_end() { end = std::chrono::steady_clock::now(); return *this; }
+    void set_start() { start = std::chrono::steady_clock::now(); };
+    std::string time_elapsed(const unsigned int divide = 1) const
+    {
+        const unsigned int s = std::max(divide, static_cast<unsigned int>(1));
+        std::chrono::milliseconds total(std::chrono::duration_cast<std::chrono::seconds>(end - start));
+        total /= s;
+        std::chrono::hours   hh = std::chrono::duration_cast<std::chrono::hours>(total);
+        total -= hh;
+        std::chrono::minutes mm = std::chrono::duration_cast<std::chrono::minutes>(total);
+        total -= mm;
+        std::chrono::seconds ss = std::chrono::duration_cast<std::chrono::seconds>(total);
+        total -= ss;
+        std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(total);
+
+        std::ostringstream out;
+        if (hh > std::chrono::hours(0))
+            out << hh.count() << "h ";
+        if (mm > std::chrono::minutes(0))
+            out << mm.count() << "m ";
+        if (ss > std::chrono::seconds(0))
+            out << ss.count() << "s";
+        else
+            out << ms.count() << "ms";
+        return out.str();
+    }
+};
 
 #endif //IMAGE_CLASSIFIER_TOOLS_H
