03f42dcaf31e61106bd5c7c855d0b8dd55dbe094
sioneptune <sioneptune@gmail.com>
2020-01-03 16:35:05 +0100

    feat(Straightener): add first version of pre-processing, display function, temporary main for tests

--- app/src/Straigthener.cpp
+++ app/src/Straigthener.cpp
@@ -6,7 +6,11 @@
 #include "tools.h"
 
 void Straigthener::processImage(Mat &image) {
+    Mat tiny, clean;
 
+    pyrDown(image, tiny, Size(image.cols/2, image.rows/2));
+    pyrDown(tiny,clean, image.size());
+    cvtColor(clean, image, COLOR_BGR2GRAY);
 }
 
 void Straigthener::findTargets(Mat &image, vector<Point> &targets) {
@@ -14,5 +18,28 @@ void Straigthener::findTargets(Mat &image, vector<Point> &targets) {
 }
 
 void Straigthener::display(Mat &image, vector<Point> &targets) {
+    for(int i=0;i < targets.size();i++){
+        circle(image,targets[i],10,Scalar(0,0,255));
+    }
+    const string wName = "Results";
+
+    namedWindow(wName, WINDOW_NORMAL);
+    resizeWindow(wName, 600,600);
+    imshow(wName, image);
+}
+
+int main(){
+    static const string name = "../../../data/00000.png";
+    Mat image = imread(name, IMREAD_COLOR);
+
+    if (image.empty()) {
+        cout << "Couldn't load " << name << endl;
+        exit(0);
+    }
+
+    Straigthener::processImage(image);
+
+    vector<Point> targets = {};
+
 
 }
