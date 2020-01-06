05fb28440dec3267df0f414f45bb886d7b3b262a
sioneptune <sioneptune@gmail.com>
2020-01-06 15:59:39 +0100

    doc(Straightener): Add the documentation for the function in the .h

--- app/include/Straightener.h
+++ app/include/Straightener.h
@@ -36,17 +36,39 @@ class Straightener {
 
 public:
 
+    /**
+     * Pre-processing function that denoises and image and converts it to grayscale
+     * @param image the image matrix we want to process
+     */
     static void processImage(Mat &image);
 
+    /**
+     * Detects the two targets in the image
+     *
+     * @param image in which we search for the targets
+     * @param targets : vector that is filled with the points the function detects as the center of each cross
+     */
     static void findTargets(Mat &image, vector<Point> &targets);
 
+    /**
+     * Debug function. Displays the image with circles around where the targets have been detected
+     * @param image to display
+     * @param targets we want to show on the image
+     */
     static void display(Mat &image, vector<Point> &targets);
 
+    /**
+     * Takes an image, detects the crosses, and rotates it so the image is straight
+     * @param image to straighten
+     */
     static void straighten(Mat &image);
 
 
 };
 
+/**
+ * Custom exception to be raised when one of the crosses is not detected in the straightening process
+ */
 struct CrossNotDetected : public std::exception
 {
     const char * what () const throw ()
