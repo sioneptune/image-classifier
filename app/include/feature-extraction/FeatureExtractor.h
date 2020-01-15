5c40f661ff468371bee79b4f0fe87f10fa32b2d3
NuagedOort <ceinture.dorbs@gmail.com>
2020-01-15 19:51:59 +0100

    feat(Feature): Creation of Feature objects to contains data during extraction. Initialization of a skeleton for FeatureExtractor. Basic Exporting method

--- app/include/feature-extraction/FeatureExtractor.h
+++ app/include/feature-extraction/FeatureExtractor.h
@@ -1,10 +1,78 @@
 #ifndef IMAGE_CLASSIFIER_FEATUREEXTRACTOR_H
 #define IMAGE_CLASSIFIER_FEATUREEXTRACTOR_H
 
+#include <iostream>
+#include <utility>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 
-class FeatureExtractor {
+using namespace cv;
+using namespace std;
 
+enum FeatureFunction { FUNCTION_BOOL, FUNCTION_STRING, FUNCTION_INT, FUNCTION_DOUBLE };
+
+class Feature {
+public:
+    const FeatureFunction name;
+    explicit Feature(FeatureFunction f): name(f) {};
+    virtual string getDescriptor() = 0;
+    virtual string getValue() = 0;
+    string getName() {
+        switch (name) {
+            case FUNCTION_BOOL: return "function_bool_or_any_name";
+            case FUNCTION_STRING: return "function_something";
+            case FUNCTION_INT: return "function_that_happened_to_be_useful";
+            case FUNCTION_DOUBLE: return "function_double_trouble";
+        }
+    }
 };
 
+class FeatureExtractor {
+private:
+    vector<Feature*> results;
+public:
+    FeatureExtractor();
+    Feature* functionBool();
+    Feature* functionString();
+    Feature* functionInt();
+    Feature* functionDouble();
+    void exportARFF(const vector<FeatureFunction>& list, string path);
+};
+
+// Inherited Features
+
+class FeatureBool : public Feature {
+public:
+    const string nominalValues = "{true, false}";
+    bool value;
+    FeatureBool(FeatureFunction f, bool val): Feature(f), value(val) {};
+    string getDescriptor() override {   return "@ATTRIBUTE "+ getName() + " " + nominalValues;    }
+    string getValue() override {    return to_string(value);    }
+};
+
+class FeatureString : public Feature {
+public:
+    const string nominalValues; // Should be in the format "{val_1, val_2, etc.}"
+    string value;
+    FeatureString(FeatureFunction f, string nominalVal, string val = ""): Feature(f), nominalValues(std::move(nominalVal)), value(std::move(val)) {};
+    string getDescriptor() override {   return "@ATTRIBUTE "+ getName() + " " + nominalValues;    }
+    string getValue() override {    return value;    }
+};
+
+class FeatureInt : public Feature {
+public:
+    int value;
+    FeatureInt(FeatureFunction f, int val): Feature(f), value(val) {};
+    string getDescriptor() override {   return "@ATTRIBUTE "+ getName() + " NUMERIC";    }
+    string getValue() override {    return to_string(value);    }
+};
+
+class FeatureDouble : public Feature {
+public:
+    double value;
+    FeatureDouble(FeatureFunction f, double val): Feature(f), value(val) {};
+    string getDescriptor() override {   return "@ATTRIBUTE "+ getName()+ " NUMERIC";    }
+    string getValue() override {    return to_string(value);    }
+};
 
 #endif //IMAGE_CLASSIFIER_FEATUREEXTRACTOR_H
--- app/src/feature-extraction/FeatureExtractor.cpp
+++ app/src/feature-extraction/FeatureExtractor.cpp
@@ -1,1 +1,59 @@
 #include "feature-extraction/FeatureExtractor.h"
+
+FeatureExtractor::FeatureExtractor() {
+    results = vector<Feature*>();
+}
+
+Feature* FeatureExtractor::functionBool() {
+    return new FeatureBool(FUNCTION_BOOL, false);
+}
+
+Feature* FeatureExtractor::functionString() {
+    return new FeatureString(FUNCTION_STRING, "{pelican, pingouin, cockatiel}", "cockatiel");
+}
+
+Feature* FeatureExtractor::functionInt() {
+    return new FeatureInt(FUNCTION_INT, 124);
+}
+
+Feature* FeatureExtractor::functionDouble() {
+    return new FeatureDouble(FUNCTION_DOUBLE, -34.56);
+}
+
+void FeatureExtractor::exportARFF(const vector<FeatureFunction> &list, string path) {
+    Feature *feat = nullptr;
+    cout << "Exportation to " + path << endl;
+    // TODO : Export the Title in the final file
+    cout << "@RELATION EXTRACTED_IMAGES\n" << endl;
+
+    // Extraction
+    for(FeatureFunction f : list) {
+        switch (f) {
+            case FUNCTION_BOOL: feat = functionBool();
+                break;
+            case FUNCTION_STRING: feat = functionString();
+                break;
+            case FUNCTION_INT: feat = functionInt();
+                break;
+            case FUNCTION_DOUBLE: feat = functionDouble();
+                break;
+        }
+        // TODO : Export the Header in the final file
+        cout << feat->getDescriptor() << endl;
+        results.push_back(feat);
+    }
+
+    // Exportation
+    // TODO : Export the Data in the final file
+    cout << "\n@DATA" << endl;
+    string tinyRes = "";
+    for(Feature* f : results) {
+        tinyRes+= f->getValue()+",";
+    }
+    cout << tinyRes << endl;
+}
+
+int a_main(){
+    FeatureExtractor feat = FeatureExtractor();
+    feat.exportARFF({ FUNCTION_BOOL, FUNCTION_INT, FUNCTION_STRING, FUNCTION_DOUBLE }, "a/path");
+}
