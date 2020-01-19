b110fd87c713a97d68ba587e258bfb8dd4ebe938
mdarfay <mdarfay@insa-rennes.fr>
2020-01-19 16:08:40 +0100

    test(FeatureExtractor): Add test for nbCircles feature

--- app/src/feature-extraction/FeatureExtractor.cpp
+++ app/src/feature-extraction/FeatureExtractor.cpp
@@ -21,8 +21,7 @@ Feature* FeatureExtractor::functionDouble(Mat& img) {
 }
 
 Feature* FeatureExtractor::nbCircles(const Mat& img) {
-    const int ARBITRARY_THRESH_HOUGH = 10;
-
+    // Image pre-processing
     Mat modif;
     cvtColor(img, modif, COLOR_BGR2GRAY);
     morphologyEx(modif, modif, MORPH_ERODE, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
@@ -30,9 +29,28 @@ Feature* FeatureExtractor::nbCircles(const Mat& img) {
 
     vector<Point> bb = boundingBox(img);
     const int width = abs(bb[0].x - bb[1].x);
+    const int ARBITRARY_THRESH_HOUGH = 10;
+    const int nbPointsNeeded = width + ARBITRARY_THRESH_HOUGH;
 
     vector<Vec3f> circles;
-    HoughCircles(modif, circles,HOUGH_GRADIENT,2, modif.rows/4,100,width + ARBITRARY_THRESH_HOUGH); //40 with small circle
+    HoughCircles(modif, circles,HOUGH_GRADIENT,2, modif.rows/4,100, nbPointsNeeded);
+
+    /* DEBUG */
+    /// Draw the circles detected
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+        int radius = cvRound(circles[i][2]);
+        // circle center
+        circle( img, center, 3, Scalar(0,255,0), -1, 8, 0 );
+        // circle outline
+        circle( img, center, radius, Scalar(0,0,255), 3, 8, 0 );
+    }
+
+    /// Show your results
+    namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
+    imshow( "Hough Circle Transform Demo", img );
+    waitKey();
 
     return new FeatureInt(NB_CIRCLES, circles.size());
 }
@@ -94,10 +112,46 @@ void FeatureExtractor::exportARFF(const vector<FeatureFunction> &list, string in
 int main(){
     FeatureExtractor feat = FeatureExtractor();
 //    feat.exportARFF({ FUNCTION_BOOL, FUNCTION_INT, FUNCTION_STRING, FUNCTION_DOUBLE }, "../../data/output/", "../../data/");
-    Mat i = openImage("../../data/output/bomb/bomb_000_18_2_4.png");
-    cout << feat.nbCircles(i)->getValue() << endl;
-    i = openImage("../../data/output/bomb/bomb_002_12_1_3.png");
-    cout << feat.nbCircles(i)->getValue() << endl;
-    i = openImage("../../data/output/bomb/bomb_034_09_5_3.png");
-    cout << feat.nbCircles(i)->getValue() << endl;
+
+    /* TESTS FOR NBCIRCLES */
+//    // Bomb
+//    Mat i = openImage("../../data/output/bomb/bomb_000_18_2_4.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/bomb/bomb_002_12_1_3.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/bomb/bomb_034_09_5_3.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//
+//    // Casualty
+//    Mat i = openImage("../../data/output/casualty/casualty_003_02_6_3.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/casualty/casualty_002_02_7_2.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/casualty/casualty_001_03_5_1.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/casualty/casualty_003_18_5_4.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//
+//    // Injury
+//    Mat i = openImage("../../data/output/injury/injury_000_01_2_1.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/injury/injury_000_04_3_2.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/injury/injury_000_15_2_3.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/injury/injury_001_09_2_5.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//
+//    // Road_block
+//    Mat i = openImage("../../data/output/road_block/road block_000_01_6_2.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/road_block/road block_000_14_4_1.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/road_block/road block_001_05_4_2.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/road_block/road block_004_03_5_4.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+//    i = openImage("../../data/output/road_block/road block_005_06_3_1.png");
+//    cout << feat.nbCircles(i)->getValue() << endl;
+
 }
